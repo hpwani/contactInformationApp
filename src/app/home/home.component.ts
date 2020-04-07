d6c12c57243bf532fa46b24588e25520ea0e4e65
hpwani <hpwani@gmail.com>
2020-04-07 20:33:48 +0530

    add info form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  addContactForm: FormGroup;
 
-  constructor() { }
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  createForm() {
+    this.addContactForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      contactNumber: ['', Validators.required],
+      emailId: ['', Validators.required],
+      address: ['', Validators.required],
+    })
+  }
+
+  addContactInfo() {
+    
   }
 
 }
